d8f525596d5d6a29e69bb3dc69a98d3ad470e6e7
Mihir-Rokde <100846891+Mihir-Rokde@users.noreply.github.com>
2023-07-10 19:46:26 +0530

    Add files via upload

--- pro156.py
+++ pro156.py
@@ -1,9 +1,12 @@
 from tkinter import*
 from  PIL import ImageTk,Image
+import random
 root=Tk()
 root.title("Endless Pokemon Card Game")
 root.geometry("600x400")
 root.configure(background="lightgreen")
+
+img=ImageTk.PhotoImage(Image.open("button.jpg"))
 
 pikachu=ImageTk.PhotoImage(Image.open("pikachu.jpg"))
 bulbasour=ImageTk.PhotoImage(Image.open("bulbasour.jpg"))
@@ -31,7 +34,41 @@ player1score.place(relx=0.1,rely=0.4,anchor=CENTER)
 player2score=Label(root,bg="orange",fg="white")
 player2score.place(relx=0.9,rely=0.4,anchor=CENTER)
 
-randomdice=Label(root,bg="yellow",fg="black")
-randomdice.place(relx=0.5,rely=0.5,anchor=CENTER)
+pokemon_list=[pikachu,bulbasour,charmender,squirtle,squirtle,nidoking,jigglypuff,meowth,persion,abra,kadabra,iyvasour]
+power_list=[200,60,100,50,50,40,70,60,70,30,70]
+label=Label(root)
+label.place(relx=0.5,rely=0.5,anchor=CENTER)
+
+player1_score=0
+player2_score=0
+
+def player1():
+    global player1_score
+    rn1=random.randint(1,6)
+    random_pokemon=pokemon_list[rn1]
+    label["image"]=random_pokemon
+    
+    random_power_list=power_list[rn1]
+    
+    player1_score=player1_score+random_power_list
+    player1score["text"]=str(player1_score)
+    
+p1btn=Button(root,image=img,command=player1)    
+p1btn.place(relx=0.1,rely=0.6,anchor=CENTER)
+
+def player2():
+    global player2_score
+    rn2=random.randint(1,6)
+    random_pokemon1=pokemon_list[rn2]
+    label["image"]=random_pokemon1
+    
+    random_power_list=power_list[rn2]
+    
+    player2_score=player2_score+random_power_list
+    player2score["text"]=str(player2_score)
+    
+p2btn=Button(root,image=img,command=player2)    
+p2btn.place(relx=0.9,rely=0.6,anchor=CENTER)
+
 
 root.mainloop()
